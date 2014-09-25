ec1071aa01999fc9047315e80f81ac5afe1ba544
Matt Ludwigs <matt@mattludwigs.com>
2014-09-24 23:37:37 -0700

    server port updates

--- app/templates/_server.js
+++ app/templates/_server.js
@@ -1,12 +1,15 @@
 var express = require("express"),
 		routes = require("./app/routes"),
-		app = express();
+		app = express(),
+		port;
 
 // Include the routes module		
 routes(app);
+// Set port
+port = process.env.PORT || 9778;
 // Use public directory for static files
 app.use("/public", express.static(__dirname + "/public/"));
 
 // Your code here
 
-app.listen("9778");
+app.listen(port);
