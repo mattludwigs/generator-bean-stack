b52da1eb1be598b98f8cd656f353bebe94876262
Matt Ludwigs <matthew.ludwigs@meltmedia.com>
2014-09-24 17:02:42 -0700

    made tests

--- test/test-load.js
+++ test/test-load.js
@@ -4,7 +4,7 @@
 var assert = require('assert');
 
 describe("bean stack", function () {
-  it('can be imported without blowing up', function () {
+  it("can be imported without blowing up", function () {
     var app = require('../app');
     assert(app !== undefined);
   });
